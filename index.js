15d843711ee3f2d97d6b8a34f8a27f59bf757848
Alex <119153127+Amack2@users.noreply.github.com>
2023-04-01 23:25:29 -0700

    continued project for extra credit, playing with getting the sign up button to disable when inputs are invalid

--- index.js
+++ index.js
@@ -1,29 +1,50 @@
 const password = document.querySelector('#password');
 const confirmPassword = document.querySelector('#cPassword');
-
-
-
 const submitBtn = document.querySelector(".submitBtn")
+const email = document.querySelector('#email');
+
 
 
 password.addEventListener("focusout", (event) => {
-    if (confirmPassword.value.length > 0) { doThePasswordsMatch(); }
+    if (confirmPassword.value.length > 0) {
+        doThePasswordsMatch();
+        testies();
+    }
 }
 );
 
 confirmPassword.addEventListener("focusout", (event) => {
     doThePasswordsMatch();
+    testies();
 }
 );
 
 function doThePasswordsMatch() {
     if (password.value === confirmPassword.value) {
-        console.log("they do")
-    } else {
+        password.setAttribute("class", "validRow");
+        confirmPassword.setAttribute("class", "validRow")
+        submitBtn.disabled = false;
+        } else {
         password.setAttribute("class", "invalidRow")
         confirmPassword.setAttribute("class", "invalidRow")
-    
+        submitBtn.disabled = true;
+               
     }
 };
 
+email.addEventListener("focusout", (event) => {
+    testies();
+})
 
+function testies() {
+    if (email.value === "") {
+        submitBtn.disabled = true;
+    }
+}
+
+testies();
+
+    
+
+// // if password rows are class -> invalid row or if the other rows are invalid than disable the button
+// document.getElementById("myBtn").disabled = true;
