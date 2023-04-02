23e07a72a0615f8a971d8077313095076782d0f5
Alex <119153127+Amack2@users.noreply.github.com>
2023-04-01 23:29:28 -0700

    project with out disabled buttons

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 const password = document.querySelector('#password');
 const confirmPassword = document.querySelector('#cPassword');
 const submitBtn = document.querySelector(".submitBtn")
-const email = document.querySelector('#email');
+
 
 
 
 password.addEventListener("focusout", (event) => {
     if (confirmPassword.value.length > 0) {
         doThePasswordsMatch();
-        testies();
     }
 }
 );
 
 confirmPassword.addEventListener("focusout", (event) => {
     doThePasswordsMatch();
-    testies();
 }
 );
 
@@ -24,27 +22,10 @@ function doThePasswordsMatch() {
         password.setAttribute("class", "validRow");
         confirmPassword.setAttribute("class", "validRow")
         submitBtn.disabled = false;
-        } else {
+    } else {
         password.setAttribute("class", "invalidRow")
         confirmPassword.setAttribute("class", "invalidRow")
         submitBtn.disabled = true;
-               
     }
 };
 
-email.addEventListener("focusout", (event) => {
-    testies();
-})
-
-function testies() {
-    if (email.value === "") {
-        submitBtn.disabled = true;
-    }
-}
-
-testies();
-
-    
-
-// // if password rows are class -> invalid row or if the other rows are invalid than disable the button
-// document.getElementById("myBtn").disabled = true;
